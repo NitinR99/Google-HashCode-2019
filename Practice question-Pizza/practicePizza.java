083dcc7da167282bbe6ea693e1e7472e59c7641b
Bilal Malik <45467181+bilalmalik4321@users.noreply.github.com>
2020-02-18 18:57:15 -0500

    Input parsing

--- Practice question-Pizza/practicePizza.java
+++ Practice question-Pizza/practicePizza.java
@@ -1,13 +1,35 @@
 import java.io.*;
 import java.util.*;
+
 class practicePizza
 {
     //parse input file
     //calculations
     //write to output file
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Github got this.");
+
+        String input;
+        String[] inputs;
+        BufferedReader br = new BufferedReader(new FileReader("input\\a_example.in"));
+        input=br.readLine();
+
+        inputs=input.split(" ");
+
+        int max = Integer.parseInt(inputs[0]);
+        int types = Integer.parseInt(inputs[1]);
+
+        System.out.println(max+" "+types);
+
+        input=br.readLine();
+        inputs=input.split(" ");
+
+       int type0 = Integer.parseInt(inputs[0]);
+       int type1 = Integer.parseInt(inputs[1]);
+       int type2 = Integer.parseInt(inputs[2]);
+       int type3 = Integer.parseInt(inputs[3]);
+       System.out.println(type0+" "+type1+" "+type2+" "+type3);
+
     }
 }
