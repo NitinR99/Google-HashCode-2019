48e54144d41b6c6a7dd47e481cb4b5769b54229c
Nitin <rameshnitin99@gmail.com>
2020-02-17 18:48:30 -0800

    skeleton

--- Practice question-Pizza/practicePizza.java
+++ Practice question-Pizza/practicePizza.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.util.*;
 class practicePizza
 {
+    //parse input file
+    //calculations
+    //write to output file
+
     public static void main(String[] args)
     {
         System.out.println("Github got this.");
