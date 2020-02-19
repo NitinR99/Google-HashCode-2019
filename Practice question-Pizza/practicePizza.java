831590055ac71257fe0bb45f9a5517bf37a34195
Nitin <rameshnitin99@gmail.com>
2020-02-18 22:22:14 -0800

    change in output

--- Practice question-Pizza/practicePizza.java
+++ Practice question-Pizza/practicePizza.java
@@ -1,51 +1,44 @@
 import java.io.*;
 import java.util.*;
-
 class practicePizza
 {
-    static int[] calc(int max, int types, int[] inputs)
+    static LinkedList<Integer> calc(int max, int types, LinkedList<Integer> inputs)
     {
         int sum=0;
         LinkedList<Integer> arr= new LinkedList<Integer>();
-        for(int i=inputs.length-1;i>=0;i--)
+        for(int i=inputs.size()-1;i>=0;i--)
         {
-            if(!((sum+inputs[i])>max))
+            if(!((sum+inputs.get(i))>max))
             {
-                sum+=inputs[i];
-                arr.add(inputs[i]);
+                sum+=inputs.get(i);
+                arr.add(inputs.get(i));
             }
         }
         Collections.sort(arr);
-        int b[]=arr.stream().mapToInt(Integer::intValue).toArray();
-        return b;
+        return arr;
     }
-
     public static void main(String[] args) throws IOException
     {
-
         String input;
-        int[] inputs;
-        BufferedReader br = new BufferedReader(new FileReader("input\\d_quite_big.in"));
+        LinkedList<Integer> inputs=new LinkedList<Integer>();
+        BufferedReader br = new BufferedReader(new FileReader("input\\a_example.in"));
         input=br.readLine();
         StringTokenizer st=new StringTokenizer(input);
-
         int max = Integer.parseInt(st.nextToken());
         int types = Integer.parseInt(st.nextToken());
-        inputs=new int[types];
         input=br.readLine();
         st=new StringTokenizer(input);
-        int i=0;
         while(st.hasMoreTokens())
         {
-            inputs[i++]=Integer.parseInt(st.nextToken());
+            inputs.add(Integer.parseInt(st.nextToken()));
         }
-
-        int result[]=calc(max,types,inputs);
-        PrintWriter pw=new PrintWriter(new FileWriter("q4.txt"));
-        pw.println(result.length);
-        for(int k=0;k<result.length;k++)
+        LinkedList<Integer> result=calc(max,types,inputs);
+        PrintWriter pw=new PrintWriter(new FileWriter("q1.txt"));
+        pw.println(result.size());
+        for(int k=0;k<result.size();k++)
         {
-            pw.print(result[k]+" ");
+            //print output in terms of index in input
+            pw.print(inputs.indexOf(result.get(k))+" ");
         }
         pw.close();
     }
