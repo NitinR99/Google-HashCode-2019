5e5d123c686eb9f6de9bdfdce9c632b01c31342e
Bilal Malik <45467181+bilalmalik4321@users.noreply.github.com>
2020-02-20 14:04:52 -0500

    Update googleBooks.java

--- Question 1/googleBooks.java
+++ Question 1/googleBooks.java
@@ -1,5 +1,10 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 class Library
 {
@@ -19,73 +24,72 @@ class Library
     {
         return "Library id:"+id+"\nNumber of books:"+no_books+"\nSignup days:"+signup_days+"\nShipping capacity:"+shipcap+"";
     }
+    
+    
 }
-
-
 public class googleBooks
 {
-    
-    static int Book, Libraries, Days;
-     private static ArrayList<Integer> Input, Output;
-    
-    public static void main(String[] args)
+	static int counter=0;
+    public static void main(String[] args) throws IOException
     {
-       
-        
-        
-        String[] fileNames = {"a_example", "b_read_on", "c_incunabula", "d_tough_choices", "e_so_many_books"};
-        
-        for(int i =0; i< fileNames.length; i++)
-        {
-            
-            
-            getInputFromFile(fileNames[i]);
-            
-            
-            
-            
-        }
-    
+    		int i = 0;
+    		int k = 1;
+    		int val1;
+    		int val2;
+    		int val3;
+    		String c;
+    		String lib;
+    		String liby[];
+    		Library libber;
+    		Scanner br = new Scanner(new FileReader("C:\\Users\\a_example (1).txt"));
+    		StringTokenizer st;
+    		String gen = br.nextLine();
+    		String scores = br.nextLine();
+    		ArrayList <Library> libList = new ArrayList<Library>();
+    		Set<Integer> set = new HashSet<Integer>();
+    		
+    		
+    		
+    		while(br.hasNextLine()){
+    			lib = br.nextLine(); 
+    			st = new StringTokenizer(lib);
+    			while(st.hasMoreTokens()) {
+    				c=st.nextToken();
+    				val1=Integer.parseInt(c);
+    				c=st.nextToken();
+    				val2=Integer.parseInt(c);
+    				c=st.nextToken();
+    				val3=Integer.parseInt(c);
+    				libber = new Library(counter,val1, val2, val3);
+    				
+    				int k=calc(libber);
+    				
+    				libList.add(libber);
+    			}
+    			
+    			lib = br.nextLine();
+    			st = new StringTokenizer(lib);
+    			
+    			while(st.hasMoreTokens()) {
+    				c=st.nextToken();
+    				val1=Integer.parseInt(c);
+    				k*=val1;
+    				set.add(val1);
+    				
+    				
+    			}
+    			i++;
+    		}
+    		
+    		
     }
     
-    
-    static void getInputFromFile(String fileName) throws FileNotFoundException
-    {
-        Input = new ArrayList<Integer>();
-        
-        BufferedReader fr = new BufferedReader(new FileReader(fileName + ".txt"));
-        
-        String line, firstLine;
-        firstLine = fr.readLine();
-        String[] vars;
-        vars = firstLine.split(" ");
-        
-        Books = Integer.valueOf(vars[0]); // the number of books
-        Libraries = Integer.valueOf(vars[1]); // the number of libraries
-        Days = Integer.valueOf(vars[2]); // the number of days for signup
-        
-        System.out.println("======= input of " + fileName);
-        System.out.println(Books + " " + Libraries + " " Num);
-        
-        while((line = fr.readLine()) != null)
-        {
-            
-            String scores[] line.split(" ");
-            
-            for(int i = 0; i < Books.length; i++)
-            {
-                Input.add(Integer.valueOf(scores[i]));
-                System.out.print(scores[i] + " ");
-            }
-        }
-        
-        
-        
+    /* (num og boof*shipcap)/sindays + bookpointstotal */
+    public static int calc(Library libber) {
+    	int k=0;
+    	
+    	libber.
+    	
+    	return k;
     }
-    
-    
-    
-    
-    
-    
 }
