02e9e1fda1c7b8a522256077accae312e7250020
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-02-20 13:23:45 -0500

    update

--- Question 1/googleBooks.java
+++ Question 1/googleBooks.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.*;
+
 class Library
 {
     int no_books;
@@ -6,10 +8,49 @@ class Library
     int shipcap;
     ArrayList<Integer> scoreBooks;
 }
+
+
 public class googleBooks
 {
+    
+    static int Book, Libraries, Days;
+    
     public static void main(String[] args)
     {
-
+        private static ArrayList<Integer> Input, Output;
+        
+        
+        String[] fileNames = {"a_example", "b_read_on", "c_incunabula", "d_tough_choices", "e_so_many_books"};
+        
+        for(int i =0; i< fileNames.length; i++)
+        {
+            
+            
+            getInputFromFile(fileNames[i]);
+            
+            
+            
+            
+        }
+    
     }
+    
+    
+    static void getInputFromFile(String fileName) throws FileNotFoundException
+    {
+        Input = new ArrayList<Integer>();
+        
+        BufferedReader fr = new BufferedReader(new FileReader(fileName + ".txt"));
+        
+        String line, f
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
