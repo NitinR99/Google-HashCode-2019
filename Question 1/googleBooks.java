0d9295978139d8a6099dc0d24d92c61bbdfb0248
Nitin <rameshnitin99@gmail.com>
2020-02-20 13:05:13 -0800

    init

---
+++ Question 1/googleBooks.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Library
+{
+    int no_books;
+    int signup_days;
+    int shipcap;
+    ArrayList<Integer> scoreBooks;
+}
+public class googleBooks
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
