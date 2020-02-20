c41f1c5d95c6ef3f06b89c5de717ffd96d1d86d4
Nitin <rameshnitin99@gmail.com>
2020-02-20 13:15:16 -0800

    init

--- Question 1/googleBooks.java
+++ Question 1/googleBooks.java
@@ -1,10 +1,22 @@
 import java.io.*;
 class Library
 {
+    int id;
     int no_books;
     int signup_days;
     int shipcap;
     ArrayList<Integer> scoreBooks;
+    Library(int identity, int numberbooks, int signupdays, int shippingcap)
+    {
+        id=identity;
+        no_books=numberbooks;
+        signup_days=signupdays;
+        shipcap=shippingcap;
+    }
+    public String toString()
+    {
+        return "Library id:"+id+"\nNumber of books:"+no_books+"\nSignup days:"+signup_days+"\nShipping capacity:"+shipcap+"";
+    }
 }
 public class googleBooks
 {
